dcd46824ffd2fc9fc8135d3fb58c1eee31f0516c
NettoJM <nettojm86@gmail.com>
2023-08-22 15:10:17 -0300

    Add class "PessoaCandidata", add JavaDocs

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,12 +1,27 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * Classe abstrata que representa uma pessoa no sistema de votação.
+ * Esta classe serve como uma classe base para tipos de pessoas específicas.
+ */
 public abstract class Pessoa {
-  protected String nome;
 
+  protected String nome; // Nome da pessoa
+
+  /**
+   * Obtém o nome da pessoa.
+   *
+   * @return O nome da pessoa.
+   */
   public String getNome() {
     return nome;
   }
 
+  /**
+   * Define o nome da pessoa.
+   *
+   * @param nome O nome a ser definido para a pessoa.
+   */
   public void setNome(String nome) {
     this.nome = nome;
   }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,84 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Representa uma pessoa candidata em um sistema de votação.
+ * Esta classe estende a classe Pessoa e adiciona funcionalidades específicas para candidatos.
+ */
+public class PessoaCandidata extends Pessoa {
 
+  private int numero; // Número de identificação da pessoa candidata
+  private int votos;  // Quantidade de votos recebidos pela pessoa candidata
+
+  /**
+   * Construtor da classe PessoaCandidata.
+   *
+   * @param nome   O nome da pessoa candidata.
+   * @param numero O número de identificação da pessoa candidata.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  /**
+   * Incrementa a contagem de votos recebidos pela pessoa candidata.
+   */
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  /**
+   * Obtém o número de identificação da pessoa candidata.
+   *
+   * @return O número de identificação.
+   */
+  public int getNumero() {
+    return numero;
+  }
+
+  /**
+   * Define o número de identificação da pessoa candidata.
+   *
+   * @param numero O número de identificação a ser definido.
+   */
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  /**
+   * Obtém a quantidade de votos recebidos pela pessoa candidata.
+   *
+   * @return A quantidade de votos recebidos.
+   */
+  public int getVotos() {
+    return votos;
+  }
+
+  /**
+   * Define a quantidade de votos recebidos pela pessoa candidata.
+   *
+   * @param votos A quantidade de votos a ser definida.
+   */
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  /**
+   * Obtém o nome da pessoa candidata.
+   *
+   * @return O nome da pessoa candidata.
+   */
+  public String getNome() {
+    return nome;
+  }
+
+  /**
+   * Define o nome da pessoa candidata.
+   *
+   * @param nome O nome a ser definido para a pessoa candidata.
+   */
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
